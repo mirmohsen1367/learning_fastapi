6c1e333a334550aa317603f1b00b5062245dba7a
mirmohsen@gmail.com <mirmohsen@gmail.com>
2026-09-15 23:51:50 +0330

    Add background task notification example

--- routers/items.py
+++ routers/items.py
@@ -1,56 +1,25 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
-
-
-# arouter = APIRouter(prefix="/items", tags=["items"])
-
-# def create_test(username: str, password: str):
-#     return {"username": username, "password": password}
-
-# class Authservice:
-#     def __init__(self, secret=None, secret_1=None):
-#         self.secret = secret
-#         self.secret_1 = secret_1
-
-# @router.get("/test_id/{test_id}")
-# async def test(test_id: int, test: Authservice =Depends(Authservice)):
-#     return test.secret_1
-# async def test(test=Depends(create_test)):
-#     return {"test": test}
-
-# ----------------------------------------------------------------------------------
-# add nested dependencies
+from fastapi import APIRouter, Depends, BackgroundTasks
 
 router = APIRouter(prefix="/items", tags=["items"])
 
-# Runs first because second_dependency needs its result.
-def first_dependency(test):
-    print("1. first_dependency runs")
-    return "Hello"
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
 
 
-# # FastAPI passes the first dependency's return value into message.
-# def second_dependency(message: Annotated[str, Depends(first_dependency)]):
-#     print("2. second_dependency runs")
-#     return message + " from items"
+def get_query(background_task: BackgroundTasks, q: str | None = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_task.add_task(write_log, message)
+    return q
 
 
-# @router.get("/{test_id}")
-# def read_items(test_id: Annotated[str, Depends(first_dependency)]):
-#     print("3. read_items runs")
-#     return {"message": test_id}
-
-
-def first_dependency(ss: str):
-    return f"processed-{ss}"
-
-
-@router.get("/{test_id}")
-def read_items(
-    test_id: str,
-    result: Annotated[str, Depends(first_dependency)],
+@router.post("/send-notification/{email}")
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: Annotated[str, Depends(get_query)]
 ):
-    return {
-        "test_id": test_id,
-        "result": result,
-    }
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent"}
